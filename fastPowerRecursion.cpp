5bc8fbbe130800ba049c22163085c20486bed745
Guptaverse <sggreat88@gmail.com>
2022-01-07 16:10:04 +0530

    update1

--- fastPowerRecursion.cpp
+++ fastPowerRecursion.cpp
@@ -12,12 +12,20 @@ int fastPower(int a, int n){
     if(n==1){
         return a;
     }
-    if(n%2==0){
-        return fastPower(a,n/2) * fastPower(a,n/2);
+    // if(n%2==0){
+    //     return fastPower(a,n/2) * fastPower(a,n/2);
+    // }
+    // else{
+    //     return a * fastPower(a,(n-1)/2) * fastPower(a,(n-1)/2);
+    // }
+
+    //Another way 
+    int smallerAns = fastPower(a,n/2);
+    smallerAns *= smallerAns;
+    if(n&1){              // here i am using bit masking to check the number is odd for example if n=5 its binary is 101 & 001 => 001 if no is odd then its result will always 1
+        return a * smallerAns;
     }
-    else{
-        return a * fastPower(a,(n-1)/2) * fastPower(a,(n-1)/2);
-    }
+    return smallerAns;
 }
 
 
